1503a41765edb5d860d5323620de7e87a1220946
letu27112008 <164725677+letu27112008@users.noreply.github.com>
2024-05-05 16:01:35 +0700

    Add files via upload

--- bai tap.py
+++ bai tap.py
@@ -1,32 +1,3 @@
-# matkhau = input("nhap pass:")
-# while matkhau == 'python':
-#     print ('hello')
-#     break
-# else:
-#     print ('nhap lai')
-
-# n = int(input("nhap n :"))
-# for i in range (2, n-1):
-#     if n%i ==0:
-#         print('ko la snt')
-#         break
-# else:
-#         print('la snt')
-# score = 0
-# ans1 = input("Python duoc phat hanh vao nam nao A.1991, B.1989, C.2000, D.1995 ")
-# if ans1 == 'A':
-#         print('dung') 
-#         score =score + 1
-# ans2 = input("Ai la nguoi tao ra python A.Guido Van Rossum, B.Dennis Ritchie, C.James Gosling, D.Bjarne Stroustrup")
-# if ans2 == 'A':
-#         print('dung') 
-#         score =score + 1
-# ans3 = input("Cau len nao dung de hien thi du lieu ra man hinh trong python A.print(), B.cout<<, C.System, D.echo ")
-# if ans3 == 'A':
-#         print('dung') 
-#         score =score + 1
-# print (score)
-
 print('chao mung ban den voi tro choi')
 point = 0
 ans1 = input("Con sông nào dài nhất bán đảo Đông Dương:\nA.Sông Hồng\nB.Sông Thầy\nC.Mê Kông\nD.Sông Ấn")
